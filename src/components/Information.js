e1e223def598bdc137e61b9a9c77a5115cf1d926
ekenecf <ekens2u@gmail.com>
2022-10-17 17:05:40 +0100

    Add more properties to the information.js file

--- src/components/Information.js
+++ src/components/Information.js
@@ -1,4 +1,10 @@
-import image1 from '../assets/calculator.png'
+import image1 from '../assets/calculator.png';
+import image2 from '../assets/hotel.png';
+import image3 from '../assets/budget_app_desktop.png';
+import image4 from '../assets/space-x.png';
+import image5 from '../assets/churchAPP_API.png';
+import image6 from '../assets/dictionary_online.png';
+import image7 from '../assets/covid19_snapshot.png';
 
 const info = {
   miniBio: [ // these are just some "tidbits" about yourself. You can look at mine https://paytonjewell.github.io/#/ for an example if you'd like
@@ -56,37 +62,43 @@ const info = {
       title: 'Project 1',
       live: 'https://micro-hotel-reservation.netlify.app/', // this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
       source: 'https://github.com/ekenecf/hotel-reservation-frontend', // this should be a link to the **repository** of the project, where the code is hosted.
-      // image: mock1
+      image: image2,
     },
     {
       title: 'Project 2',
       live: 'https://ekenebudgetapp.herokuapp.com/',
       source: 'https://github.com/ekenecf/Budget-App',
-      // image: mock2
+      image: image3,
     },
     {
       title: 'Project 3',
       live: 'https://gorgeous-melba-746edd.netlify.app/',
       source: 'https://github.com/ekenecf/spacex',
-      // image: mock3
+      image: image4,
     },
     {
       title: 'Project 4',
       live: 'https://stcharlescyon.herokuapp.com/',
       source: 'https://github.com/ekenecf/my-church-app',
-      // image: mock4
+      image: image5,
     },
     {
       title: 'Project 5',
       live: 'https://incandescent-puffpuff-380218.netlify.app/',
       source: 'https://github.com/ekenecf/react-dictionary',
-      // image: mock5
+      image: image6,
     },
     {
-      title: 'Project 5',
+      title: 'Project 6',
+      live: 'https://warm-panda-37f6ed.netlify.app/',
+      source: 'https://github.com/ekenecf/covid19-datatracking-app',
+      image: image7,
+    },
+    {
+      title: 'Project 7',
       live: 'https://ekenemathmagician.netlify.app/',
       source: 'https://github.com/ekenecf/math-magician',
-      image: image1
+      image: image1,
     },
   ],
 };
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -12,25 +12,25 @@ function Portfolio() {
       {
             info.portfolio.map((portfolioInfo) => (
               <div className="portfolio_card" key={uuid()}>
-                 <div className="container">
-        <div className="card">
-          <div className="slide slide1">
-            <div className="content">
-              <div className="icon">
-                <img src={portfolioInfo.image} alt="portfolioImage" />
-              </div>
-            </div>
-          </div>
-          <div className="slide slide2">
-            <div className="content">
-              <h3>
-                {portfolioInfo.title}
-              </h3>
-              <p>Trust yourself and keep going.</p>
-            </div>
-          </div>
-        </div>
-      </div>
+                <div className="container">
+                  <div className="card">
+                    <div className="slide slide1">
+                      <div className="content">
+                        <div className="icon">
+                          <img src={portfolioInfo.image} alt="portfolioImage" />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="slide slide2">
+                      <div className="content">
+                        <h3>
+                          {portfolioInfo.title}
+                        </h3>
+                        <p>Trust yourself and keep going.</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             ))
           }
