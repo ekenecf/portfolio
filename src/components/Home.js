457472bab1d88f7c3791c98b81681586a694c446
ekenecf <ekens2u@gmail.com>
2023-01-30 20:42:00 +0100

    Add Framer

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { FiTwitter } from 'react-icons/fi';
 import { FaLinkedin, FaGithub } from 'react-icons/fa';
 import { NavLink } from 'react-router-dom';
+import { motion } from 'framer-motion';
+
 import './Home.css';
 import info from './Information';
 import myImage from '../assets/my_image-removebg-preview.png';
@@ -19,10 +21,21 @@ function Home() {
 
   return (
     <div className="home_card">
-      <div className="developer_image">
+      <motion.div
+        initial={{ opacity: 0, x: '-100vh' }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ type: 'spring', bounce: 0.2 }}
+        className="developer_image"
+      >
         <img src={myImage} alt="developerImage" />
-      </div>
-      <div className="developer_short_detail">
+      </motion.div>
+
+      <motion.div
+        initial={{ opacity: 0, x: '100vh' }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ type: 'spring', bounce: 0.6 }}
+        className="developer_short_detail"
+      >
         <h1>
           Hi, I&apos;m
           {' '}
@@ -30,21 +43,18 @@ function Home() {
           <span className="hand_wave">🤚</span>
         </h1>
         <h2>I&apos;m a Full Stack Developer.</h2>
-        {
-            info.miniBio.map((bio) => (
-              <div key={bio.id} className="developers_bio">
-                <p>{bio.emoji}</p>
-                <span>{bio.text}</span>
-              </div>
-            ))
-        }
+        {info.miniBio.map((bio) => (
+          <div key={bio.id} className="developers_bio">
+            <p>{bio.emoji}</p>
+            <span>{bio.text}</span>
+          </div>
+        ))}
         <div className="developers_socials">
           <h3 onClick={twitter} onKeyDown={twitter} role="presentation">
             {' '}
             <FiTwitter />
           </h3>
           <h3 onClick={github} onKeyDown={github} role="presentation">
-
             {' '}
             <FaGithub />
           </h3>
@@ -54,9 +64,20 @@ function Home() {
             <FaLinkedin />
           </h3>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 }
 
 export default Home;
+  <motion.div
+    initial={{ opacity: 0, x: '-100vh' }}
+    animate={{ opacity: 1, x: 0 }}
+    transition={{ type: 'spring', bounce: 0.6 }}
+  >
+    <h1>Hi</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+      perspiciatis voluptates nihil dolores eum architecto eligendi
+    </p>
+  </motion.div>;
